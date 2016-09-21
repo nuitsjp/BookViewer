27eb150f92119f419e74ed1788543b612089092c
nuits.jp <nuits.jp@live.jp>
2016-09-21 14:00:10 +0900

    Bookコンストラクタのテストケースの追加

---
+++ BookViewer/BookViewer.Tests/Models/BookTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookViewer.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookViewer.Tests.Models
+{
+    [TestClass]
+    public class BookTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var expected = new Book();
+            Assert.IsNull(expected.CurrentChapter);
+            Assert.IsNull(expected.CurrentPage);
+            Assert.IsNotNull(expected.Chapters);
+            Assert.AreEqual(0, expected.Chapters.Count);
+        }
+    }
+}
